475eb897528c56840940b6c208c47f2ac3696ce1
Axel Dolce <axel.dolce@gmail.com>
2017-05-15 22:39:49 -0300

    login & firebase user

--- src/App.js
+++ src/App.js
@@ -11,12 +11,16 @@ import './App.css';
 import 'antd/dist/antd.min.css'
 
 import store from './store'
+import './store-firebase'
 
 import UserToken from './containers/UserToken'
 import BulbsList from './containers/BulbsList'
 import Bulb from './containers/Bulb'
 import ScenesList from './containers/ScenesList'
 import APICalls from './components/APICalls'
+
+import Login from './containers/Login'
+import UserAttribute from './containers/UserAttribute'
 
 var Router = HashRouter
 
@@ -46,22 +50,20 @@ class App extends Component {
 							<Icon className="trigger" type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} onClick={this.toggle} />
 						)}>
 							<Menu theme="dark" mode="inline" onClick={f => f} defaultSelectedKeys={['1']} defaultOpenKeys={['sub_token', 'pages']}>
-								<div>asd</div>
-								<MenuItem>
+								<MenuItem key="account">
 									<Link to={'/account'}>
 										<Icon type="user" />
 										<span className="nav-text">Account</span>
 									</Link>
 								</MenuItem>
 								<SubMenu key="pages" title={<span><Icon type="desktop" /><span className="nav-text">Views</span></span>}>
-									<MenuItem>
+									<MenuItem key="lights">
 										<Link to={'/lights'}>Lights</Link>
 									</MenuItem>
-									<MenuItem>
+									<MenuItem key="scenes">
 										<Link to={'/scenes'}>Scenes</Link>
 									</MenuItem>
 								</SubMenu>
-								<APICalls />
 							</Menu>
 						</Sider>
 						<Layout>
@@ -69,16 +71,22 @@ class App extends Component {
 								<Content style={{ padding: '1rem', minHeight: '280px' }}>
 									<Route path={'/account'} exact render={props => {
 										return (
-											<div style={{ display: 'flex', alignItems: 'center', padding: '0.5rem' }}>
-												<div style={{ marginRight: '0.25rem' }}>
-													<UserToken />
-												</div>
+											<div>
 												<div>
-													<Tooltip placement="right" title={(
-														<p>You can generate an access token in your <a href="https://cloud.lifx.com/settings" target="_blank">LIFX account settings</a>.</p>
-													)}>
-														<Icon type="question-circle" />
-													</Tooltip>
+													<Login />
+												</div>
+												<UserAttribute name="email" />
+												<div style={{ display: 'flex', alignItems: 'center', padding: '0.5rem' }}>
+													<div style={{ marginRight: '0.25rem' }}>
+														<UserToken />
+													</div>
+													<div>
+														<Tooltip placement="right" title={(
+															<p>You can generate an access token in your <a href="https://cloud.lifx.com/settings" target="_blank">LIFX account settings</a>.</p>
+														)}>
+															<Icon type="question-circle" />
+														</Tooltip>
+													</div>
 												</div>
 											</div>
 										)
